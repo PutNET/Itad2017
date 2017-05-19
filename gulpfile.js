7fa2fc66f40887fc2537990ea9a7b9c988fbcfa8
Radosław Waberski <rjwaberski@gmail.com>
2017-05-19 19:17:52 +0200

    setup part2

--- gulpfile.js
+++ gulpfile.js
@@ -1,52 +1,26 @@
 var gulp = require('gulp');
-var rename = require('gulp-rename');
-var sourcemaps = require('gulp-sourcemaps');
-var uglify = require('gulp-uglify');
 var sass = require('gulp-sass');
-var minifyCss = require('gulp-clean-css');
+var browserSync = require('browser-sync').create();
+
+gulp.task('browserSync', function(){
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+});
 
 gulp.task('sass', function() {
-    gulp.src('./jquery.fullpage.scss')
-        .pipe(sass({
-            outputStyle: 'expanded'
-        }))
-        .pipe(gulp.dest('.'));
+    return gulp.src('src/styles/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('dist'))
+    .pipe(browserSync.reload({
+        stream: true
+    }));
 });
 
-gulp.task('css', function() {
-    gulp.src('./jquery.fullpage.css')
-        .pipe(sourcemaps.init())
-        .pipe(gulp.dest('./dist'))
-        .pipe(minifyCss({
-            compatibility: 'ie8',
-            advanced: false,
-            keepSpecialComments: '1'
-        }))
-        .pipe(rename({suffix: '.min'}))
-        .pipe(sourcemaps.write('.')) 
-        .pipe(gulp.dest('./dist'));
-});
 
-gulp.task('js', function() {
-    gulp.src('./jquery.fullpage.js')
-        .pipe(sourcemaps.init())
-        .pipe(gulp.dest('./dist'))
-        .pipe(uglify({
-            preserveComments: 'license'
-        }))
-        .pipe(rename({suffix: '.min'}))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('./dist'));
+gulp.task('watch', ['browserSync', 'sass'], function(){
+    gulp.watch('src/styles/*.scss', ['sass']);
+    gulp.watch('./index.html', browserSync.reload);
 });
-
-//private file
-gulp.task('extensions', function() {
-    gulp.src('./jquery.fullpage.extensions.js')
-        .pipe(uglify({
-            preserveComments: 'license'
-        }))
-        .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('./dist'));
-});
-
-gulp.task('default', ['css', 'js']);
