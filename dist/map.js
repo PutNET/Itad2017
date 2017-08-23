1a0b7b93b33eb1d3c0c888cd924affb07cbb97e1
Oskar <madorgon@gmail.com>
2017-08-23 18:53:23 +0200

    Wylaczony scroll na mapie

--- dist/map.js
+++ dist/map.js
@@ -2,6 +2,7 @@ function initMap() {
         var uluru = {lat: 52.403837, lng: 16.949819};
         var map = new google.maps.Map(document.getElementById('map'), {
           zoom: 16,
+          scrollwheel: false,
           center: uluru
         });
         var marker = new google.maps.Marker({
